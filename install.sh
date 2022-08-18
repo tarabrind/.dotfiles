be2f652af6d8847d0cf7201aa12c7a6bd14b7880
TDen <tarabrind@gmail.com>
2022-08-18 14:03:44 +0400

    added full installation script

--- install.sh
+++ install.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
+# Install programms
+sudo apt install \
+    stow \
+    neovim \
+    tmux \
+    zsh \
+    git \
+    curl \
+    fzf \
+    ripgrep
+
+curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Stow dotfiles
 stow git
 stow nvim
 stow tmux
 stow zsh
 
-# Use zsh as default shell
+# Use zsh as a default shell
 sudo chsh -s $(which zsh) $USER
 
 # Bundle zsh plugins 
